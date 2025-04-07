92ca9e8b71f7a9b7e82e058f48e9042592d5e77a
iodnakww <githubkhusus334@gmail.com>
2025-04-07 16:32:49 +0700

    Update embed.js

--- src/commands/Community/embed.js
+++ src/commands/Community/embed.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('disc
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('embed')
+        .setName('system-embed')
         .setDescription('Send a fully customizable embed message to the channel')
         .addStringOption(option =>
             option.setName('title')
@@ -43,7 +43,7 @@ module.exports = {
         .addStringOption(option =>
             option.setName('timestamp')
                 .setDescription('Include timestamp? (yes/no)')
-                .setRequired(false))
+                .setRequired(false)
                 .addChoices(
                     { name: 'Yes', value: 'yes' },
                     { name: 'No', value: 'no' }
@@ -64,29 +64,3 @@ module.exports = {
 
         // Create the embed
         const embed = new EmbedBuilder()
-            .setTitle(title)
-            .setDescription(description)
-            .setColor(color);
-
-        // Add optional footer
-        if (footer) embed.setFooter({ text: footer });
-
-        // Add image if URL provided
-        if (imageUrl) embed.setImage(imageUrl);
-
-        // Add thumbnail if URL provided
-        if (thumbnailUrl) embed.setThumbnail(thumbnailUrl);
-
-        // Add author details if provided
-        if (author) {
-            embed.setAuthor({ name: author, url: authorUrl, iconURL: authorIcon });
-        }
-
-        // Add timestamp if requested
-        if (timestampChoice) embed.setTimestamp();
-
-        // Send the embed to the channel
-        await interaction.reply({ content: "Your full embed has been sent!", ephemeral: true });
-        await interaction.channel.send({ embeds: [embed] });
-    }
-};
