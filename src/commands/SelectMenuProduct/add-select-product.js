de294fcbb3e6b714ed655d95be89d80c60773624
iodnakww <githubkhusus334@gmail.com>
2025-04-09 18:31:52 +0700

    Update add-select-product.js

--- src/commands/SelectMenuProduct/add-select-product.js
+++ src/commands/SelectMenuProduct/add-select-product.js
@@ -1,40 +1,50 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require("discord.js");
 const selectmenuSchema = require("../../schemas/SelectMenuSchema");
-const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    premiumOnly: false,
     data: new SlashCommandBuilder()
         .setName("add-select-product")
-        .setDescription("Add a custom select menu option.")
-        .setDMPermission(false)
+        .setDescription("Add a custom select menu option for a specific channel.")
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles)
-        .addStringOption(option =>
+        .addChannelOption(option => 
+            option.setName("channel")
+                .setDescription("Select the channel for this product")
+                .setRequired(true)
+        )
+        .addStringOption(option => 
             option.setName("label")
                 .setDescription("Label for the select menu option.")
                 .setRequired(true)
         )
-        .addStringOption(option =>
+        .addStringOption(option => 
             option.setName("description")
                 .setDescription("Description of the select menu option.")
                 .setRequired(true)
         )
-        .addStringOption(option =>
+        .addStringOption(option => 
             option.setName("emoji")
                 .setDescription("Emoji for the select menu option.")
                 .setRequired(true)
         )
-        .addStringOption(option =>
+        .addStringOption(option => 
             option.setName("response")
                 .setDescription("Response message for the select menu option.")
                 .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName("image_response")
+                .setDescription("An image URL for the response (optional).")
+                .setRequired(false)
         ),
+        
     async execute(interaction) {
         const { options, guildId } = interaction;
-
+        const channel = options.getChannel("channel");
         const label = options.getString("label");
         const description = options.getString("description");
         const emoji = options.getString("emoji");
         const response = options.getString("response");
+        const imageResponse = options.getString("image_response");
 
         try {
             const data = await selectmenuSchema.findOne({ GuildID: guildId });
@@ -44,28 +54,27 @@ module.exports = {
                 description: description || "No description provided.",
                 emoji: emoji || "",
                 response: response,
+                imageResponse: imageResponse || null, // Store the image response URL if provided
             };
 
             if (data) {
-                const optionExists = data.options.find((x) => x.label === label);
-
-                if (optionExists) {
-                    return interaction.reply({ content: `Option with label \`${label}\` already exists.`, ephemeral: true });
+                const channelData = data.channels.find(ch => ch.channelID === channel.id);
+                if (channelData) {
+                    channelData.options.push(newOption);
                 } else {
-                    data.options = [...data.options, newOption];
+                    data.channels.push({ channelID: channel.id, options: [newOption] });
                 }
-
                 await data.save();
             } else {
                 await selectmenuSchema.create({
                     GuildID: guildId,
-                    options: [newOption],
+                    channels: [{ channelID: channel.id, options: [newOption] }],
                 });
             }
 
             return interaction.reply({ 
                 embeds: [new EmbedBuilder()
-                    .setDescription(`Added new select menu option Product: **${label}**`)
+                    .setDescription(`Added new select menu option for channel **${channel.name}**: **${label}**`)
                     .setColor("Green")
                     .setTimestamp()
                 ], 
@@ -75,5 +84,5 @@ module.exports = {
             console.error(err);
             return interaction.reply({ content: "An error occurred while saving the select menu option.", ephemeral: true });
         }
-    }
+    },
 };
