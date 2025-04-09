9f8c5d8f9f9d041d2d2e94e34968227938e8445e
iodnakww <githubkhusus334@gmail.com>
2025-04-09 18:32:48 +0700

    Update SelectMenuSchema.js

--- src/schemas/SelectMenuSchema.js
+++ src/schemas/SelectMenuSchema.js
@@ -1,15 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const selectMenuSchema = new mongoose.Schema({
     GuildID: { type: String, required: true },
-    options: [
+    channels: [
         {
-            label: { type: String, required: true },
-            description: { type: String },
-            emoji: { type: String },
-            response: { type: String, required: true },
+            channelID: { type: String, required: true },
+            options: [
+                {
+                    label: { type: String, required: true },
+                    description: { type: String, required: true },
+                    emoji: { type: String, default: null },
+                    response: { type: String, required: true },
+                    imageResponse: { type: String, default: null },  // Image URL for response
+                }
+            ],
         }
     ],
 });
 
-module.exports = mongoose.model("SelectMenuSchema", selectMenuSchema);
+module.exports = mongoose.model("SelectMenu", selectMenuSchema);
